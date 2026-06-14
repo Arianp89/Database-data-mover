492e0346a4ed1a14a370e55e1a9d8513d6f4f37c
Arian <arianpanahi6@gmail.com>
2026-06-14 15:29:02 +0330

    this is fisrt commit

---
+++ information-database.py
@@ -0,0 +1,9 @@
+database_name = input('enter your database_name:')
+host = input('enter database host address:')
+user = input('enter database user:')
+password = input('enter database password:')
+db_config = {'host':host , 'user':user , 'password':password}
+
+
+
+
